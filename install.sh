65e8e4ac3f7460eae5ad23a045c648b0b972c7d6
NejcKle <klemencic.nejcc@gmail.com>
2021-09-28 08:33:49 +0100

    Update install.sh, .gitignore

--- install.sh
+++ install.sh
@@ -13,10 +13,6 @@ cd mjpg-streamer/mjpg-streamer-experimental
 echo "Installing mjpeg-streamer"
 make
 sudo make install
-# sudo echo "start_x=1 \
-# gpu_mem=128 \
-# enable_uart=1 \
-# dtparam=i2c_arm=on" > /boot/config.txt
 
 # 2. copy device tree configuration
 echo "Copying device tree configuration to /boot/dt-blob.bin"
@@ -31,6 +27,10 @@ sudo cp ./koruza_v2_driver/data.json ./koruza_v2_driver/data
 
 sudo mkdir ./config
 sudo cp config.json ./config
+sudo cp factory_defaults.json ./config/factory_defaults.json
+sudo chattr -i ./config/factory_defaults.json
+
+sudo mkdir ./images
 
 echo "Copying koruza services to /etc/systemd/system/"
 sudo cp /home/pi/koruza_v2/services/gpio_config.service /etc/systemd/system/gpio_config.service
@@ -38,12 +38,13 @@ sudo cp /home/pi/koruza_v2/services/video_stream.service /etc/systemd/system/vid
 sudo cp /home/pi/koruza_v2/services/koruza_main.service /etc/systemd/system/koruza_main.service
 sudo cp /home/pi/koruza_v2/services/koruza_ui.service /etc/systemd/system/koruza_ui.service
 sudo cp /home/pi/koruza_v2/services/koruza_d2d.service /etc/systemd/system/koruza_d2d.service
+sudo cp /home/pi/koruza_v2/services/koruza_cloud.service /etc/systemd/system/koruza_cloud.service
 
 echo "Starting all services"
 sudo systemctl daemon-reload
-sudo systemctl enable koruza_main koruza_ui koruza_d2d gpio_config video_stream
-sudo systemctl start koruza_main koruza_ui koruza_d2d gpio_config video_stream
+sudo systemctl enable koruza_main koruza_ui koruza_d2d koruza_cloud gpio_config video_stream
+sudo systemctl start koruza_main koruza_ui koruza_d2d koruza_cloud gpio_config video_stream
 
 echo "Rebooting RPi for configuration to take effect"
-sleep 5
+sleep 1
 sudo reboot now
