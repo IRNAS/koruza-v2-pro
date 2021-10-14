94d7a78dd6dfcc869ec58f76e0d3147bfb0c38ef
NejcKle <klemencic.nejcc@gmail.com>
2021-10-14 09:44:46 +0100

    Add set_config method

--- src/config_manager.py
+++ src/config_manager.py
@@ -12,6 +12,17 @@ def get_config():
     """Return read config"""
     return load_json_file(CONFIG_FILENAME)
 
+def set_config(key, value):
+    """Set config of (key, value) pair"""
+    config = load_json_file(CONFIG_FILENAME)
+    try:
+        with FileLock(CONFIG_FILENAME + ".lock"):
+            with open(CONFIG_FILENAME, "w") as config_file:
+                config[key] = value
+                json.dump(config, config_file, indent=4)
+    except Exception as e:
+        print(f"Error setting {key}:{value}: {e}")
+
 def load_json_file(filename):
     """Loads json file"""
     with FileLock(filename + ".lock"):
